7244c349ed501349ff71acd2cd3d62ae9b86b043
Dinu-Filip <dinu.filip.self@gmail.com>
2026-06-29 07:40:37 +0100

    feat(evaluator): Implement evaluation for anomaly localisation

--- bbox.py
+++ bbox.py
@@ -4,11 +4,18 @@ from PIL import Image
 
 @dataclass
 class BBox:
-    x: float
-    y: float
+    x1: float
+    y1: float
+    x2: float
+    y2: float
     width: float
     height: float
+    confidence: float
     label: str = ""
+
+    @staticmethod
+    def from_xywh(x: float, y: float, width: float, height: float, confidence: float, label: str = "") -> "BBox":
+        return BBox(x1=x, y1=y, x2=x + width, y2=y + height, width=width, height=height, confidence=confidence, label=label)
 
 
 class BBoxParser(ABC):
--- evaluation.py
+++ evaluation.py
@@ -1,17 +1,38 @@
 from bbox import BBoxParser, BBox
 from nova_dataset import NovaDataset
-
+from torch import tensor, zeros
+from torchmetrics.detection import MeanAveragePrecision
 
 class Evaluation:
-    _LOCALISATION_PROMPT = "Detect and localise all abnormalities in this brain MRI scan."
+    def __init__(self, dataset: NovaDataset):
+        self.dataset = dataset
 
-    @classmethod
-    def eval_anomaly_localisation(cls, dataset: NovaDataset, model: BBoxParser):
-        for row in dataset:
-            image = dataset.load_image(row["image_path"])
-            gt_bboxes = [
-                BBox(x=b["x"], y=b["y"], width=b["width"], height=b["height"], label=b["label"])
-                for b in row["bboxes"]
-            ]
-            pred_bboxes = model.localise_anomalies(image, Evaluation._LOCALISATION_PROMPT)
-            
+    def eval_anomaly_localisation(self, bbox_preds: list[BBox]) -> MeanAveragePrecision:
+        gt = []
+        preds = []
+        
+        for row in self.dataset:
+            image = self.dataset.load_image(row["image_path"])
+            gt_bboxes = row["bboxes"]
+            gt_boxes = tensor([
+                [b["x"], b["y"], b["x"] + b["width"], b["y"] + b["height"]]
+                for b in gt_bboxes
+            ]) if gt_bboxes else zeros((0, 4))
+            gt.append(dict(
+                boxes=gt_boxes,
+                labels=tensor([0] * len(gt_bboxes)),
+            ))
+
+            pred_boxes = tensor([
+                [b.x1, b.y1, b.x2, b.y2] for b in bbox_preds
+            ]) if bbox_preds else zeros((0, 4))
+            pred_scores = tensor([b.confidence for b in bbox_preds])
+            preds.append(dict(
+                boxes=pred_boxes,
+                scores=pred_scores,
+                labels=tensor([0] * len(bbox_preds)),
+            ))
+
+        map = MeanAveragePrecision(box_format="xyxy", iou_type="bbox")
+        map.update(preds, gt)
+        return map
